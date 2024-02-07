cadf4e8333ca07c9940b85157796e86c37bb7f65
Marcelo <marcello159357@gmail.com>
2024-02-07 15:55:33 -0300

    Filtrando episódios pelo ano.

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -3,12 +3,15 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.services.ConsumoApi;
 import br.com.alura.screenmatch.services.ConverteDados;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -34,14 +37,32 @@ public class Principal {
                 temporadas.add(dadosTemporada);
             }
             //temporadas.forEach(t-> t.episodios().forEach(e -> System.out.println(e.titulo())));
-            System.out.println("****Top 5 episódios *******");
-            List<DadosEpisodio> dadosEpisodios = temporadas.stream()
-                    .flatMap(t->t.episodios().stream())
-                    .filter(e-> !e.avaliacao().equalsIgnoreCase("N/A"))
-                    .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
-                    .limit(5)
-                    .collect(Collectors.toList());
-            dadosEpisodios.forEach(System.out::println);
+//            System.out.println("****Top 5 episódios *******");
+//            temporadas.stream()
+//                    .flatMap(t->t.episodios().stream())
+//                    .filter(e-> !e.avaliacao().equalsIgnoreCase("N/A"))
+//                    .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+//                    .limit(5)
+//                    .forEach(System.out::println);
+            List<Episodio> episodios = temporadas.stream()
+                                    .flatMap(t->t.episodios().stream()
+                                    .map(d-> new Episodio(t.numero(),d))
+                                    ).collect(Collectors.toList());
+
+
+            System.out.println("A partir de que ano você deseja ver espisódios?");
+            var ano = leitura.nextInt();
+            leitura.nextLine();
+            LocalDate dataBusca = LocalDate.of(ano,1,1);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            episodios.stream()
+                    .filter(e-> e.getDataLancamento() !=null && e.getDataLancamento().isAfter(dataBusca))
+                    .forEach(e->{
+                        System.out.println(
+                                "Temporada: "+e.getTemporada()+
+                                ", Episódio:"+e.getTitulo()+
+                                ", Data de lançamento:"+e.getDataLancamento().format(formatter));
+                    });
         }
 //        List<String> nomes = Arrays.asList("Jacque", "Iasmin", "Paulo", "Rodrigo", "Nico");
 //        String reduce = nomes.stream()
